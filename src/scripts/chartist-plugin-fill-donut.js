c5d7aaf328d21789d1558461ef78b07e5f0c9288
moxx <mario@mookey.at>
2016-10-29 20:47:23 +0200

    more jshint save syntax

--- src/scripts/chartist-plugin-fill-donut.js
+++ src/scripts/chartist-plugin-fill-donut.js
@@ -4,7 +4,7 @@
  * author-url: https://github.com/moxx/chartist-plugin-fill-donut
  *
  */
-(function(window, document, Chartist) {
+(function (document, Chartist) {
     'use strict';
 
     var defaultOptions = {
@@ -17,26 +17,27 @@
     };
 
     Chartist.plugins = Chartist.plugins || {};
-    Chartist.plugins.fillDonut = function(options) {
+    Chartist.plugins.fillDonut = function (options) {
         options = Chartist.extend({}, defaultOptions, options);
-        return function fillDonut(chart){
-            if(chart instanceof Chartist.Pie) {
+        return function fillDonut(chart) {
+            if (chart instanceof Chartist.Pie) {
                 var $chart = chart.container;
                 $chart.style.position = 'relative';
                 var $svg;
 
-                function drawDonut(data){
-                    if(data.type == 'slice'){
-                        if(data.index == 0)
+                function drawDonut(data) { // jshint ignore:line
+                    if (data.type === 'slice') {
+                        if (data.index === 0) {
                             $svg = $chart.querySelector('svg');
+                        }
 
-                        var $clone = data.group._node.cloneNode(true);
-                        options.fillClass.split(" ").forEach(function(className) {
+                        var $clone = data.group._node.cloneNode(true); // jshint ignore:line
+                        options.fillClass.split(" ").forEach(function (className) {
                             $clone.setAttribute('class', $clone.getAttribute('class') + ' ' + className);
                         });
 
-                        [].forEach.call($clone.querySelectorAll('path'), function(el){
-                            [].forEach.call(el.querySelectorAll('animate'), function(node) {
+                        [].forEach.call($clone.querySelectorAll('path'), function (el) {
+                            [].forEach.call(el.querySelectorAll('animate'), function (node) {
                                 node.parentNode.removeChild(node);
                             });
 
@@ -48,25 +49,26 @@
                     }
                 }
 
-                chart.on('draw', function(data) {
+                chart.on('draw', function (data) {
                     drawDonut(data);
                 });
 
-                chart.on('created', function(data){
+                chart.on('created', function (data) {
                     var itemIndex = 0;
 
-                    if(chart.options.fillDonutOptions){
+                    if (chart.options.fillDonutOptions) {
                         options = Chartist.extend({}, options, chart.options.fillDonutOptions);
                         drawDonut(data);
                     }
 
-                    [].forEach.call(options.items, function(thisItem){
+                    [].forEach.call(options.items, function (thisItem) {
                         var $wrapper = document.createElement(options.label.html);
-                        options.label.class.split(" ").forEach(function(className) {
-                             if ($wrapper.classList)
-                              $wrapper.classList.add(className);
-                            else
-                              $wrapper.className += ' ' + className;
+                        options.label.class.split(" ").forEach(function (className) {
+                             if ($wrapper.classList) {
+                                 $wrapper.classList.add(className);
+                             } else {
+                                 $wrapper.className += ' ' + className;
+                             }
                         });
                         var item = Chartist.extend({}, {
                             class: '',
@@ -77,18 +79,19 @@
                             offsetX: 0 //left, right in px
                         }, thisItem);
 
-                        var content = item.content;
 
-                        if(item.id.length > 0)
+                        if (item.id.length > 0) {
                             $wrapper.setAttribute('id', item.id);
-                        if(item.class.length > 0)
+                        }
+                        if (item.class.length > 0) {
                             $wrapper.setAttribute('class', item.class);
+                        }
 
-                        [].forEach.call($chart.querySelectorAll('*[data-fill-index$="fdid-'+itemIndex+'"]'), function(node){
+                        [].forEach.call($chart.querySelectorAll('*[data-fill-index$="fdid-' + itemIndex + '"]'), function (node) {
                             node.parentNode.removeChild(node);
                         });
-                        $wrapper.setAttribute('data-fill-index','fdid-'+itemIndex);
-                        itemIndex++;
+                        $wrapper.setAttribute('data-fill-index', 'fdid-' + itemIndex);
+                        itemIndex += 1;
 
                         $wrapper.insertAdjacentHTML('beforeend', item.content);
                         $wrapper.style.position = 'absolute';
@@ -102,23 +105,23 @@
                         var style = {
                             bottom: {
                                 bottom: 0 + item.offsetY + "px",
-                                left: (cWidth - wWidth) + item.offsetX + "px",
+                                left: (cWidth - wWidth) + item.offsetX + "px"
                             },
                             top: {
-                                top: 0  + item.offsetY + "px",
-                                left: (cWidth - wWidth) + item.offsetX + "px",
+                                top: 0 + item.offsetY + "px",
+                                left: (cWidth - wWidth) + item.offsetX + "px"
                             },
                             left: {
                                 top: (cHeight - wHeight) + item.offsetY + "px",
-                                left: 0 + item.offsetX + "px",
+                                left: 0 + item.offsetX + "px"
                             },
                             right: {
                                 top: (cHeight - wHeight) + item.offsetY + "px",
-                                right: 0 + item.offsetX + "px",
+                                right: 0 + item.offsetX + "px"
                             },
                             center: {
                                 top: (cHeight - wHeight) + item.offsetY + "px",
-                                left: (cWidth - wWidth) + item.offsetX + "px",
+                                left: (cWidth - wWidth) + item.offsetX + "px"
                             }
                         };
 
@@ -126,7 +129,6 @@
                     });
                 });
             }
-        }
-    }
-
-}(window, document, Chartist));
+        };
+    };
+}(document, Chartist)); // jshint ignore:line
